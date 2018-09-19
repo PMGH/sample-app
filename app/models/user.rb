53c22d2c3f7c24490cc465436cf399ef4c14c9e4
Peter McCready <petermccready2@gmail.com>
2018-09-19 22:32:22 +0100

    use more robust regex for email format validity

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # ApplicationRecord(abstract) inherits form ActiveRecord::Base
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # note regular expression above has one minor weakness: it allows invalid addresses that contain consecutive dots, such as foo@bar..com
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
