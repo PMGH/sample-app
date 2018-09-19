99d7475ef2ccd1c74e22427832b3b8f416d78207
Peter McCready <petermccready2@gmail.com>
2018-09-19 22:29:07 +0100

    test user email formatted correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   # ApplicationRecord(abstract) inherits form ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
 end
