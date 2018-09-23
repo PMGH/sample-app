854c72e9126ee18acf858e2ee5c30dba181a835c
Peter McCready <petermccready2@gmail.com>
2018-09-23 11:55:08 +0100

    test email saved as lower-case

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   # Callbacks
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
   # Validations
   validates :name, presence: true, length: { maximum: 50 }
