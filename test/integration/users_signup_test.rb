245d765c8eeac0d2b3d7d521946cf4db04e98475
Peter McCready <petermccready2@gmail.com>
2018-09-23 18:56:52 +0100

    rename sign_up route to signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
-    get sign_up_path
+    get signup_path
     assert_no_difference "User.count" do
       post users_path, params: {
         user: {
@@ -17,7 +17,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   end
 
   test "sign up error messages" do
-    get sign_up_path
+    get signup_path
     assert_no_difference "User.count" do
       post users_path, params: {
         user: {
