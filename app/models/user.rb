cd0cc2a778971a9af312850e37c3068e9e4e3d6b
Peter McCready <petermccready2@gmail.com>
2018-10-01 20:05:59 +0100

    add new_token generator

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,9 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  # return a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 end
