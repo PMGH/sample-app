b716e51ee6f1bb4ada4593355f6b6600bdee52f3
Peter McCready <petermccready2@gmail.com>
2018-10-02 21:06:33 +0100

    test successful edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   # bcrypt password validation
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # return hash digest of the given string
   def self.digest(string)
