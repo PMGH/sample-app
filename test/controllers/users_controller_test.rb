6fbf0099c9335e9c85fe43c7bc002f98e45b7a6a
Peter McCready <petermccready2@gmail.com>
2018-10-07 19:01:21 +0100

    update test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -33,12 +33,18 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
-  test "cannot update admin attribute" do
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+
     patch user_path(@other_user), params: {
       user: {
+        password: 'password',
+        password_confirmation: 'password',
         admin: true
       }
     }
+    @other_user.reload
     assert_not @other_user.admin?
   end
 end
