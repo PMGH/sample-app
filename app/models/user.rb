d0ff1ef375d318be71768f720103062992d4766c
Peter McCready <petermccready2@gmail.com>
2018-10-08 20:59:03 +0100

    add activation_token method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
   # ApplicationRecord(abstract) inherits form ActiveRecord::Base
+  attr_accessor :remember_token, :activation_token
 
-  attr_accessor :remember_token
+  # Callbacks
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   # Constants
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-
-  # Callbacks
-  before_save { email.downcase! }
 
   # Validations
   validates :name, presence: true, length: { maximum: 50 }
@@ -41,5 +41,16 @@ class User < ApplicationRecord
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
